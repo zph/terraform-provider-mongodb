ef591030c66b88f3f740a47f2bc4b27fad4fed79
Zander Hill <zander@xargs.io>
2024-05-18 16:04:22 -0700

    Experiment with composable modules

---
+++ examples/generator.ts
@@ -0,0 +1,115 @@
+import * as template from 'npm:nunjucks';
+import { snakeCase, kebabCase } from 'npm:change-case';
+
+// TODO: make sure throw works, it's not currently
+// there are many issues in repo about this :-/
+// Because it's unreliable we'll trust typescript typing instead
+const env = template.configure('.', {throwOnUndefined: true})
+
+env.addFilter('to_resource', function (str: string) {
+  if(!str) throw new Error('to_resource: str is empty');
+  return snakeCase(str.toLowerCase());
+});
+
+env.addFilter('kebab_case', function (str: string) {
+  if(!str) throw new Error('kebab_case: str is empty');
+  return kebabCase(str.toLowerCase());
+});
+
+// deno-lint-ignore no-explicit-any
+function render(tmpl: string, args: {[key: string]: any}) {
+  return env.render(tmpl, args)
+}
+
+type Node = {
+  name: string,
+  host: string,
+  port: number,
+  userAdmin: string,
+  userAdminPassword: string,
+}
+
+const DefaultNode: Node = {
+  name: 'default',
+  host: 'localhost',
+  port: 27017,
+  userAdmin: 'admin',
+  userAdminPassword: '<PASSWORD>',
+}
+
+const Server = (name: string, opts: Partial<Node> = {}): Node => {
+  const baseServer = {
+    name: snakeCase(name),
+  }
+
+  return {
+    ...DefaultNode,
+    ...opts,
+    ...baseServer,
+  }
+}
+
+type User = {
+  name: string,
+  password: string,
+}
+
+const users: User[] = [
+  {
+    name: 'admin2',
+    password: '<PASSWORD>',
+  },
+  {
+    name: 'user2',
+    password: '<PASSWORD>',
+  },
+  {
+    name: 'user3',
+    password: '<PASSWORD>',
+  },
+  {
+    name: 'user4',
+    password: '<PASSWORD>',
+  },
+  {
+    name: 'user5',
+    password: '<PASSWORD>',
+  },
+]
+const servers = {
+  'mongos': {
+    host: 'localhost',
+    port: 27017,
+    userAdmin: 'admin',
+    userAdminPassword: 'admin',
+  },
+  'shard-01': {
+    host: 'localhost',
+    port: 27021,
+    userAdmin: 'admin',
+    userAdminPassword: 'admin',
+  },
+  'shard-02': {
+    host: 'localhost',
+    port: 27023,
+    userAdmin: 'admin',
+    userAdminPassword: 'admin',
+  },
+  'shard-03': {
+    host: 'localhost',
+    port: 27026,
+    userAdmin: 'admin',
+    userAdminPassword: 'admin',
+  },
+}
+
+const main = () => {
+  const args: {nodes: Node[], users: User[]} = {
+    nodes: Object.entries(servers).map(([name, opts]) => Server(name, opts)),
+    users
+  }
+  const hcl = render('main.tf.j2', args)
+  console.log(hcl)
+}
+
+main()
